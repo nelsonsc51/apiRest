06fc75180d1d424d8c4ba6ec8ea53e5f337451f7
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-09-06 14:13:49 -0300

    Criação do projeto APIREST com Maven, WEB, JPA e DevTools

---
+++ moneyapi/src/test/java/com/example/moneyapi/MoneyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
