f717aa01f80b9e3098e22a60391542b8a5fb9707
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-09-10 00:01:18 -0300

    Criação da CategoriaResource

---
+++ moneyapi/src/main/java/com/example/moneyapi/resource/CategoriaResource.java
@@ -0,0 +1,10 @@
+package com.example.moneyapi.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+}
