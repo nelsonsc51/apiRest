e17817b5984fa59c581dc7a26f506a9d145b6989
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-09-10 00:24:09 -0300

    variável do tipo JPAPrepository e mapeamento para listar categoria

--- moneyapi/src/main/java/com/example/moneyapi/resource/CategoriaResource.java
+++ moneyapi/src/main/java/com/example/moneyapi/resource/CategoriaResource.java
@@ -1,10 +1,28 @@
 package com.example.moneyapi.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.moneyapi.model.Categoria;
+import com.example.moneyapi.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		return categoriaRepository.findAll();
+	}
+	
+	
 
 }
